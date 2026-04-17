aa7c63dc8326416303c0821bbb173fbed39a8f39
Reece <reecemorgandev@gmail.com>
2026-04-17 16:31:12 +0100

    fixed int division warning

--- godot-project/game/global/product_manager/product_manager.gd
+++ godot-project/game/global/product_manager/product_manager.gd
@@ -6,11 +6,14 @@ var items_to_restock: Array[ProductPlaceholder] = []
 
 func restock_products() -> void:
 	var no_of_products: int = items_to_restock.size()
-	var max_range: int = randi_range(no_of_products/2, no_of_products)
+	# Use bitshift for halving ints
+	# equivalent of /2, but stops int warning
+	var min_range: int = no_of_products >> 1
+	var max_range: int = randi_range(min_range, no_of_products)
 	
 	items_to_restock.shuffle()
 	
-	for i in max_range:
+	for i in range(max_range):
 		print("Restocking ", items_to_restock[i].product_data.product_info.product_name)
 		items_to_restock[i].visible = true
 		items_to_restock[i].collision_shape.disabled = false
