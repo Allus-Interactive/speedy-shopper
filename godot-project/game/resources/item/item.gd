b46a800e7f6f80101c6d48335b34ce859ea709e5
Reece <reecemorgandev@gmail.com>
2026-04-17 15:30:25 +0100

    added new product type and new resources

--- godot-project/game/resources/item/item.gd
+++ godot-project/game/resources/item/item.gd
@@ -7,4 +7,13 @@ class_name Item
 @export var product_price: float = 0
 @export var product_type: CATEGORY
 
-enum CATEGORY { BEER, BREAD, CEREAL, CRISPS, FIZZY_DRINK, MILK, SPIRITS, TINNED_FOOD, WINE }
+enum CATEGORY { BEER, BREAD, CEREAL, CRISPS, FIZZY_DRINK, MILK, SPIRITS, TINNED_FOOD, WINE, CONDIMENTS }
+
+# TODO: implement into CATEGORY 
+# { MEAT, FRESH, PASTA_RICE, COOKING_SAUCES, MEAL_KITS }
+# Aisle 1, Left Side, in order left to right
+# FRESH: cheese, yoghurts, ready meals, deserts etc.
+# MEAT: sausages,burgers, steak etc.
+# PASTA_RICE: PAsta, Rice, etc. 
+# COOKING_SAUCES: Pasta sauce, curry sauce etc.
+# MEAL_KITS: fajita kit, enchilada kit, taco kit etc.
