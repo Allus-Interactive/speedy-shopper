2c5f28bf6487ee4b658f437d44d3be956ea8e258
Reece <reecemorgandev@gmail.com>
2026-05-05 15:14:09 +0100

    removed commented code

--- godot-project/game/resources/item/item.gd
+++ godot-project/game/resources/item/item.gd
@@ -8,12 +8,3 @@ class_name Item
 @export var product_type: CATEGORY
 
 enum CATEGORY { BEER, BREAD, CEREAL, CRISPS, FIZZY_DRINK, MILK, SPIRITS, TINNED_FOOD, WINE, CONDIMENTS, MEAT, FRESH, MEAL_KITS, PASTA_RICE, COOKING_SAUCES }
-
-# TODO: implement into CATEGORY 
-# { MEAT, FRESH, PASTA_RICE, COOKING_SAUCES, MEAL_KITS }
-# Aisle 1, Left Side, in order left to right
-# FRESH: cheese, yoghurts, ready meals, deserts etc.
-# MEAT: sausages,burgers, steak etc.
-# PASTA_RICE: PAsta, Rice, etc. 
-# COOKING_SAUCES: Pasta sauce, curry sauce etc.
-# MEAL_KITS: fajita kit, enchilada kit, taco kit etc.
