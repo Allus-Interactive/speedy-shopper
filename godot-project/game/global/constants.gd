fd2459a83d31cf69c87b2d3e02441f58e22106d1
Reece <reecemorgandev@gmail.com>
2026-08-03 10:55:10 +0100

    added settings menu

--- godot-project/game/global/constants.gd
+++ godot-project/game/global/constants.gd
@@ -6,3 +6,4 @@ const DRIVING_SCENE: String = "uid://hdoipf1qndch"
 const TUTORIAL_SCENE: String = "uid://d3pjk0siutk0r"
 
 const TITLE_SCREEN: String = "uid://by64gyav16ry7"
+const SETTINGS_SCREEN: String = "uid://bvnccs7ugunpy"
--- godot-project/game/screens/title_screen/title_screen.gd
+++ godot-project/game/screens/title_screen/title_screen.gd
@@ -9,3 +9,6 @@ func _on_tutorial_button_pressed() -> void:
 	TutorialManager.tutorial_enabled = true
 	
 	GameManager.thread_load_scene(Constants.SHOP_SCENE)
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file(Constants.SETTINGS_SCREEN)
