20dc61b638d0bb33442b6af0a989b45d951f237d
Reece Morgan <112851652+Reece-Morgan@users.noreply.github.com>
2026-08-19 17:29:51 +0100

    add settings option for 24 hour clock

--- godot-project/game/screens/settings_screen/settings_screen.gd
+++ godot-project/game/screens/settings_screen/settings_screen.gd
@@ -5,6 +5,7 @@ class_name SettingsScreen#
 @onready var music_slider: HSlider = $MusicSlider
 @onready var sfx_slider: HSlider = $SfxSlider
 @onready var sfx_player: SfxPlayer = $SFXPlayer
+@onready var time_check_button: CheckButton = $TimeCheckButton
 
 @onready var button_press_sfx: AudioStream = preload("res://assets/sfx/button_press.mp3")
 
@@ -17,9 +18,10 @@ func _ready() -> void:
 	# Load saved settings
 	load_settings()
 	
-	# Set sliders to current volume
+	# Set UI to current settings
 	music_slider.value = GameManager.music_volume
 	sfx_slider.value = GameManager.sfx_volume
+	time_check_button.button_pressed = GameManager.use_24_hour
 
 func save_settings() -> void:
 	config.set_value("audio", "music", music_slider.value)
@@ -39,6 +41,9 @@ func _on_sfx_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(sfx_bus, value)
 	GameManager.sfx_volume = value
 
+func _on_time_check_button_toggled(toggled_on: bool) -> void:
+	GameManager.use_24_hour = toggled_on
+	
 func _on_back_button_pressed() -> void:
 	sfx_player.play_sfx(button_press_sfx)
 	save_settings()
