b29f67e550d011546092ac4dd6e1112378701e88
Reece <reecemorgandev@gmail.com>
2026-08-20 11:08:08 +0100

    reset seconds_per_minute

--- godot-project/game/global/game_time_manager.gd
+++ godot-project/game/global/game_time_manager.gd
@@ -15,7 +15,7 @@ var hour: int = 10 # default = 10
 var minute: int = 0 # default = 0
 
 var time_accumulator : float = 0.0
-var seconds_per_minute : float = 0.05 # default = 10.0
+var seconds_per_minute : float = 5.0 # default = 5.0
 
 signal minute_changed
 signal hour_changed
