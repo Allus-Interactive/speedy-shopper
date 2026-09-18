25d2b8f9d176454562f2e3c634449f6786f9a192
Reece <reecemorgandev@gmail.com>
2026-09-18 08:28:00 +0100

    forward time at rest on fade

--- godot-project/game/environment/buildings/player_house/player_house_interaction.gd
+++ godot-project/game/environment/buildings/player_house/player_house_interaction.gd
@@ -6,6 +6,6 @@ func get_interaction_tooltip(_player: Player) -> String:
 func interact(player: Player) -> void:
 	print("Let's wait until morning")
 	player.fade_out()
-	GameTimeManager.rest()
 	await get_tree().create_timer(1.5).timeout
+	GameTimeManager.rest()
 	player.fade_in()
